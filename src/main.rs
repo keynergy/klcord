5b60eacc84c8b51aebc69c06a719e45219a823e4
semi <semilinares@pm.me>
2022-01-30 10:10:19 -0800

    show year in layout command

--- src/main.rs
+++ src/main.rs
@@ -73,11 +73,16 @@ fn display_matrix(m: &[Vec<char>]) -> String {
 }
 
 fn print_layout(l: &Layout) -> String {
+    let year = match l.year {
+	0 => "".to_string(),
+	_ => std::format!("({})", l.year)
+    };
     std::format!(
-        "**{}**\nCreated by {}\n```\n{}\n```",
-        l.name,
-        l.author,
-        display_matrix(&l.formats.standard.as_ref().unwrap().matrix)
+	"**{}**\nCreated by {} {}\n```\n{}\n```",
+	l.name,
+	l.author,
+	year,
+	display_matrix(&l.formats.standard.as_ref().unwrap().matrix)
     )
 }
 
