b79eaaee4d8593a5c3027faf0d0abd0680d5c057
semi <semilinares@pm.me>
2022-02-08 20:56:58 -0800

    add translate command

--- src/utility.rs
+++ src/utility.rs
@@ -1,22 +1,16 @@
-use rust_fuzzy_search::fuzzy_search_best_n;
 use keynergy::layout::Layout;
-use serenity::model::channel::Message;
+use rust_fuzzy_search::fuzzy_search_best_n;
 use serenity::client::Context;
+use serenity::model::channel::Message;
 
 pub async fn send_message(ctx: &Context, msg: &Message, content: impl std::fmt::Display) {
-	if let Err(why) = msg.channel_id
-    	.say(&ctx.http, content)
-    	.await
-	{
-		println!("Error sending message: {:?}", why);
-	}
+    if let Err(why) = msg.channel_id.say(&ctx.http, content).await {
+        println!("Error sending message: {:?}", why);
+    }
 }
 
 pub fn closest_match(name: String, names: &Vec<String>) -> String {
-    let names = names
-        .iter()
-        .map(String::as_ref)
-        .collect::<Vec<&str>>();
+    let names = names.iter().map(String::as_ref).collect::<Vec<&str>>();
     fuzzy_search_best_n(&name, &names, 1)[0].0.to_string()
 }
 
@@ -37,19 +31,19 @@ pub fn display_matrix(m: &[Vec<char>]) -> String {
 
 pub fn print_layout(l: &Layout) -> String {
     let year = match l.year {
-	    0 => "".to_string(),
-	    _ => std::format!("({})", l.year)
+        0 => "".to_string(),
+        _ => std::format!("({})", l.year),
     };
     let link = match &l.link {
-	    Some(x) => std::format!("<https://{}>\n", x),
-	    None => "".to_string()
+        Some(x) => std::format!("<https://{}>\n", x),
+        None => "".to_string(),
     };
     std::format!(
-	    "**{}**\nCreated by {} {}\n{}```\n{}\n```",
-	    l.name,
-	    l.author,
-	    year,
-	    link,
-	    display_matrix(&l.formats.standard.as_ref().unwrap().matrix)
+        "**{}**\nCreated by {} {}\n{}```\n{}\n```",
+        l.name,
+        l.author,
+        year,
+        link,
+        display_matrix(&l.formats.standard.as_ref().unwrap().matrix)
     )
 }
